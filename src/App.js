58b2c7bec290416b079f2fee8a39600b9f17fcd5
Joe <joseph_strauss@yahoo.com>
2017-05-29 11:06:07 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-title"><h3>Joseph Strauss</h3><h4>Full Stack Developer</h4></div>
+        </div>
+
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default App;
